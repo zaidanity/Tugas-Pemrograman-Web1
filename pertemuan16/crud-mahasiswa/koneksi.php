5f36929118ad29dfdb76a09a6d301956ac42fae1
Jidann <zaidanity@gmail.com>
2026-06-12 23:57:34 +0700

    Remove Railway config, revert koneksi to local

--- pertemuan16/crud-mahasiswa/koneksi.php
+++ pertemuan16/crud-mahasiswa/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
-$db   = getenv('DB_NAME') ?: 'db_mahasiswa';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_mahasiswa";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
